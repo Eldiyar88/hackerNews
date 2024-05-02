5e03b0383bca571084185a40025b006d45345054
Eldiyar88 <eldiyareltaev@gmail.com>
2024-05-02 15:28:47 +0300

    t

--- mini-app/src/panels/Home.js
+++ mini-app/src/panels/Home.js
@@ -3,34 +3,26 @@ import axios from 'axios';
 import { Panel, PanelHeader, Group, Div, List, Cell,  Button } from '@vkontakte/vkui';
 import PropTypes from 'prop-types';
 import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
-// import { DEFAULT_VIEW_PANELS } from '../routes';
 
 
 export const Home = ({ id }) => {
-  // const { goToPanel } = useActiveVkuiLocation();
   const routeNavigator = useRouteNavigator();
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  // console.log('---------->>>', routeNavigator);
 
-  // Функция для загрузки новостей
   const fetchNews = async () => {
     setIsLoading(true);
     try {
-      // Загружаем новости с сервера
       const response = await axios.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
       console.log('------------->>>>', response);
       const newsIds = response.data.slice(-100);
 
-      // Загружаем данные каждой новости
       const fetchedNews = await Promise.all(newsIds.map(id =>
         axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`).then(res => res.data)
       ));
 
-      // Сортируем новости по дате публикации в порядке убывания
       fetchedNews.sort((a, b) => b.time - a.time);
 
-      // Обновляем список новостей
       setNews(fetchedNews);
     } catch (error) {
       console.error('Failed to fetch news:', error);
@@ -38,41 +30,29 @@ export const Home = ({ id }) => {
     setIsLoading(false);
   };
 
-  // Функция для обработки события прокрутки
   const handleScroll = () => {
     const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
     if (scrollHeight - scrollTop === clientHeight && !isLoading) {
-      // Пользователь достиг конца страницы, загружаем следующую партию новостей
       fetchNews();
     }
   };
 
-  // Загрузка первой партии новостей при монтировании компонента
   useEffect(() => {
     fetchNews();
-        // Устанавливаем интервал для автоматического обновления новостей раз в минуту
         const interval = setInterval(fetchNews, 60000);
 
-        // Очистка интервала при размонтировании компонента
         return () => clearInterval(interval);
   }, 
   []);
 
-  // Добавляем обработчик события прокрутки при монтировании компонента
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-//   // При нажатии на новость
-// const handleNewsItemClick = (newsId) => {
-//   goToPanel(DEFAULT_VIEW_PANELS.NEWSITEM, { id: newsId }); 
-//   // Переход на страницу новости с передачей id в параметрах URL
-// };
 
-// При нажатии на новость
 const handleNewsItemClick = (newsId) => {
-  routeNavigator.push(`/newsItem/${newsId}`); // Навигация на страницу новости с передачей id в параметрах маршрута
+  routeNavigator.push(`/newsItem/${newsId}`); 
 };
 
   return (
@@ -80,7 +60,6 @@ const handleNewsItemClick = (newsId) => {
       <PanelHeader>Главная</PanelHeader>
       <Group header="Hacker news">
       <Div>
-          {/* Кнопка для принудительного обновления списка новостей */}
           <Button stretched size="l" mode="secondary" onClick={async () => {
             setIsLoading(true);
             try {
